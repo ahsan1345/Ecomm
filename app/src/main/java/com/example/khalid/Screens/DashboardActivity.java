75bfb6d94edea997c62582f44e556c3b6e36fd2f
ahsan1345 <121722151+ahsan1345@users.noreply.github.com>
2024-01-06 18:53:46 +0500

    fragment

--- app/src/main/java/com/example/khalid/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/khalid/Screens/DashboardActivity.java
@@ -2,17 +2,25 @@ package com.example.khalid.Screens;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.khalid.R;
 import com.example.khalid.Screens.Admin.AdminDashboardActivity;
+import com.example.khalid.Screens.Fragments.AccountFragment;
+import com.example.khalid.Screens.Fragments.CardFragment;
+import com.example.khalid.Screens.Fragments.FavoriteFragment;
+import com.example.khalid.Screens.Fragments.HomeFragment;
 import com.example.khalid.databinding.ActivityDashboardBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -82,6 +90,31 @@ public class DashboardActivity extends AppCompatActivity {
 
             }
         });
+       replaceFragment(new HomeFragment());
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            String title = item.getTitle().toString();
+            switch (title){
+                case "Home":
+                    replaceFragment(new HomeFragment());
+                    break;
+                case "cart":
+                    replaceFragment(new CardFragment());
+                    break;
+                case "favorite":
+                    replaceFragment(new FavoriteFragment());
+                    break;
+                case "account":
+                    replaceFragment(new AccountFragment());
+                    break;
+                    
 
+            }
+            return true;
+        });
+
+    }
+    public void replaceFragment(Fragment fragment){
+
+       getSupportFragmentManager().beginTransaction().replace(R.id.frame,fragment).commit();
     }
 }
